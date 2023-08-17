d52f542cb0f22cd93e26349032b99ad2ea6801ea
Asus <turgunboyevjurabek777@gmail.com>
2023-08-17 16:33:09 +0500

    time3

--- app/src/main/java/uz/turgunboyevjurabek/viewpagerexperiense/MainActivity.kt
+++ app/src/main/java/uz/turgunboyevjurabek/viewpagerexperiense/MainActivity.kt
@@ -1,8 +1,14 @@
 package uz.turgunboyevjurabek.viewpagerexperiense
 
+import android.annotation.SuppressLint
+import android.content.ClipData.Item
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
 import uz.turgunboyevjurabek.viewpagerexperiense.adapter.PagerAdapter
 import uz.turgunboyevjurabek.viewpagerexperiense.databinding.ActivityMainBinding
 import uz.turgunboyevjurabek.viewpagerexperiense.databinding.ItemTabBinding
@@ -10,6 +16,7 @@ import uz.turgunboyevjurabek.viewpagerexperiense.databinding.ItemTabBinding
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var pagerAdapter: PagerAdapter
+    private val tabBindings = ArrayList<ItemTabBinding>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -17,21 +24,50 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun viewPager() {
-        val fm=supportFragmentManager
-        pagerAdapter=PagerAdapter(fm)
+
+        val list= ArrayList<String>()
+        list.addAll(listOf("Junior","Middle","Senior"))
+        pagerAdapter=PagerAdapter(supportFragmentManager)
         binding.myViewPager.adapter=pagerAdapter
         binding.myTabLayout.setupWithViewPager(binding.myViewPager)
 
-        val list=ArrayList<String>()
-        list.addAll(listOf("Junior","Middle","Senior"))
+        val tabCount = binding.myTabLayout.tabCount
+        val tabItemBinding = ItemTabBinding.inflate(layoutInflater)
 
-        val itemTabBinding=ItemTabBinding.inflate(layoutInflater)
-        val tabcount=binding.myTabLayout.tabCount
-        for (i in 0 until tabcount  ){
-            val tab=binding.myTabLayout.getTabAt(i)
-            itemTabBinding.itemTabText.text=list[i]
-            tab?.text=list[i]
-            tab?.customView=itemTabBinding.root
+
+
+       binding.myTabLayout.addOnTabSelectedListener(object :OnTabSelectedListener{
+            @SuppressLint("ResourceAsColor")
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val position = tab?.position ?: 0
+                val tabBinding = tabBindings[position]
+                tabBinding.itemTabText.text = list[position]
+                tabBinding.itemTabText.setTextColor(R.color.sariq)
+                tab?.text = tabBinding.itemTabText.text.toString()
+                tab?.setCustomView(tabBinding.root)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                val position = tab?.position ?: 0
+        val tabBinding = tabBindings[position]
+        tabBinding.itemTabText.text = list[position]
+        tab?.text = tabBinding.itemTabText.text.toString()
+        tab?.setCustomView(tabBinding.root)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
+        for (i in 0 until tabCount) {
+            //   val tabItemBinding = ItemTabBinding.inflate(layoutInflater)
+            val tab = binding.myTabLayout.getTabAt(i)
+            tabItemBinding.itemTabText.text = list[i]
+            tab?.customView = tabItemBinding.root
+            tabBindings.add(tabItemBinding)
         }
+
+
     }
 }
--- app/src/main/java/uz/turgunboyevjurabek/viewpagerexperiense/adapter/PagerAdapter.kt
+++ app/src/main/java/uz/turgunboyevjurabek/viewpagerexperiense/adapter/PagerAdapter.kt
@@ -1,21 +1,41 @@
 package uz.turgunboyevjurabek.viewpagerexperiense.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
 import uz.turgunboyevjurabek.viewpagerexperiense.R
 import uz.turgunboyevjurabek.viewpagerexperiense.databinding.ActivityMainBinding
+import uz.turgunboyevjurabek.viewpagerexperiense.databinding.ItemTabBinding
 import uz.turgunboyevjurabek.viewpagerexperiense.fragment.FirstFragment
 import uz.turgunboyevjurabek.viewpagerexperiense.fragment.SecondFragment
 import uz.turgunboyevjurabek.viewpagerexperiense.fragment.ThirdFragment
 
-class PagerAdapter(val fm :FragmentManager): FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
+class PagerAdapter(val fa :FragmentManager ): FragmentStatePagerAdapter(fa) {
 
+//    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+//        val itemTabBinding=ItemTabBinding.inflate(LayoutInflater.from(container.context),container,false)
+//        itemTabBinding.itemTabText.text=list[position]
+//        container.addView(itemTabBinding.root)
+//        return itemTabBinding.root
+//    }
+//
+//    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+//        container.removeView(`object` as View)
+//    }
+
+
+    override fun getCount(): Int {
         return 3
     }
 
@@ -27,4 +47,5 @@ class PagerAdapter(val fm :FragmentManager): FragmentPagerAdapter(fm) {
             else -> FirstFragment()
         }
     }
+
 }
